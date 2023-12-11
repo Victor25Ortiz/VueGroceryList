23c9c2507632e55d4cad34d4c00538935b83af3d
Victor25Ortiz <143905805+Victor25Ortiz@users.noreply.github.com>
2023-12-11 09:01:24 -0600

    deploying pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vuegrocerylist/',
+  base: '/VueGroceryList/',
   plugins: [
     vue(),
     vueJsx(),
